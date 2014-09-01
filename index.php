db7bca854b07d9d17cd8097861b48d45d9ec338e
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2014-09-01 21:51:45 +0700

    initial

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require 'vendor/autoload.php';
+use Carbon\Carbon;
+date_default_timezone_set('Asia/Jakarta');
+$date = Carbon::createFromDate(1945, 8, 17);
+printf("Kapan Indonesia Merdeka? %s", $date->diffForHumans());

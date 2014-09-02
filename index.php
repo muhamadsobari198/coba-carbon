70fbfdf5fb4972a3f804ae709b8577ec0f1aec6a
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2014-09-02 09:23:49 +0700

    add new line

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ require 'vendor/autoload.php';
 use Carbon\Carbon;
 date_default_timezone_set('Asia/Jakarta');
 $date = Carbon::createFromDate(1945, 8, 17);
-printf("Kapan Indonesia Merdeka? %s", $date->diffForHumans());
+printf("Kapan Indonesia Merdeka? %s\n", $date->diffForHumans());
